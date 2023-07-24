f8d80dbd6f13e19dbf91d0354c5e64de469837b8
Judiesharlet <judithnjoki6@gmail.com>
2023-07-25 00:47:21 +0300

    print_char.c

--- print_char.c
+++ print_char.c
@@ -2,15 +2,13 @@
 
 /**
  * print_char - prints a character
- * @args: arguments
- *
+ * @args: arguments*
  * Return:int
  */
 
 int print_char(va_list args)
 {
 	char c;
-
 	c = va_arg(args, int);
 	_putchar(c);
 	return (1);
