412544a464f4bebdb8cec32103ef83eda7fafe49
Judiesharlet <judithnjoki6@gmail.com>
2023-07-24 23:16:26 +0300

    print_percent.c

---
+++ print_percent.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_percent - print percent_sign
+ * @args:not used
+ * Return: int
+ */
+int print_percent(_attribute_((unused))va_list *args)
+{
+	_putchar('%');
+	return (1);
+}
