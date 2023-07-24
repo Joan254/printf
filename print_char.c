1ba610cf8164d7614848266edab0a49ee18f2266
Judiesharlet <judithnjoki6@gmail.com>
2023-07-24 23:13:11 +0300

    print_percent.c

---
+++ print_char.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_char - prints a character
+ * @args: arguments
+ *
+ * Return:int
+ */
+
+ int print_char(va_list args)
+{
+	char c;
+
+	c = va_arg(args, int);
+	_putchar(c);
+	return (1);
+}
