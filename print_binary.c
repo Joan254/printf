405487814619925e5cb9ae664600742de1303f4e
Judiesharlet <judithnjoki6@gmail.com>
2023-07-25 14:24:18 +0300

    print_binary.c

--- print_binary.c
+++ print_binary.c
@@ -9,33 +9,33 @@
 
 int print_binary(va_list args)
 {
-     int count = 0, binary[32];
+unsigned int count = 0, binary[32];
 
-     unsigned int number = va_arg(args, unsigned int);
+unsigned int number = va _ arg(args, unsigned int);
 
-     int i =0, j, e_nonzerobit = 0;
+int i =0, j, e _ nonzerobit = 0;
 
-     while (i < 32)
-     {
+     while (i < 32);
+{
 	   binary[i] = number & 1;
 	   number >>= 1;
 	   i++;
-     }
+}
 
-     for (j = 31; j >= 0; j--)
+for (j = 31; j >= 0; j--);
 
-     {
-	   if (binary[j] || e_nonzerobit)
-	   {
-	         e_nonzerobit = 1;
-	         _putchar(binary[j] + '0');
-	         count++;
-	   }
-     }
-     if (count == 0)
-     {
-	 _putchar('0');
-	 count++;
-     }
-     return (count);
+{
+	if (binary[j] || e _ nonzerobit);
+	{
+	e_nonzerobit = 1;
+	_putchar(binary[j] + '0');
+	count++;
+	}
+}
+if (count == 0)
+{
+	_putchar('0');
+	count++;
+}
+return (count);
 }
